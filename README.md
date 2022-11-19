6651ac19710c0a6dd583cb1fab6d8e2bb0602e37
unknown <jimmymhuang@gmail.com>
2022-11-19 16:34:57 -0600

    buxfixing and generated README

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Work-Day-Scheduler
+# Work Day Scheduler
+![License](https://img.shields.io/badge/License-MIT-blue)
+
+## Description
+This project is a simple work day planner built using simple JavaScript and JQuery and the legacy moment.js library. The app uses localStorage to save information for users to access upon page refresh.
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+
+## Installation
+
+Click the link below for the live deployed application on GitHub.
+
+
+
+## Usage
+
+For typical work day purposes, this planner loops from 9am through 5pm and is set to today's date using Moment.js. Users can enter and save text into any of the colored fields that are coded past, present, and future, noted as grey, red, and green in that order.Users can type information into the fields and then click the save buttons to save the text into localStorage. Upon page refresh, information should be saved and still present for reading.
+
+
+
+## License
+
+Refer to license in repo or as below:
+
+MIT License
+
+Copyright (c) 2022 jimmehhuang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- assets/script.js
+++ assets/script.js
@@ -1,71 +1,146 @@
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-
-//array of each hour block
+// variable/array of each hour block to loop through the scheduler
 var dayBlocks = [
     {
-        hour: "08",
-        ampm: "am",
-    },
-    {
+        id: "0",
         hour: "09",
+        time: "09",
         ampm: "am",
+        reminder: " "
     },
     {
+        id: "1",
         hour: "10",
+        time: "10",
         ampm: "am",
+        reminder: " "
     },
     {
+        id: "2",
         hour: "11",
+        time: "11",
         ampm: "am",
+        reminder: " "
     },
     {
+        id: "3",
         hour: "12",
+        time: "12",
         ampm: "pm",
+        reminder: " "
     },
     {
+        id: "4",
         hour: "01",
+        time: "13",
         ampm: "pm",
+        reminder: " "
     },
     {
+        id: "5",
         hour: "02",
+        time: "14",
         ampm: "pm",
+        reminder: " "
     },
     {
+        id: "6",
         hour: "03",
+        time: "15",
         ampm: "pm",
+        reminder: " "
     },
     {
+        id: "7",
         hour: "04",
+        time: "16",
         ampm: "pm",
+        reminder: " "
+    },
+    {
+        id: "8",
+        hour: "05",
+        time: "17",
+        ampm: "pm",
+        reminder: " "
     }
 ]
-//function to display date at top of page/header
+
+// function to display date at top of page/header
 function todaysDate(){
-    const dateMoment = moment().format('[Today is] dddd, MMMM Do, YYYY');
+    var dateMoment = moment().format('[Today is] dddd, MMMM Do, YYYY [and it is] h:mm:ss:a');
     $("#currentDay").text(dateMoment);
 }
-todaysDate();
-//create timeblock w/ times listed
-function hourBlock(){
-    let hourRow = $("<form>").attr({"class": "row"});
-    $(".container").append(hourRow);
-}
-//show save button
-let saveButton = $("<i class='far fa-save fa-lg'></i>")
-let savePlan = $("<button>").attr({"class": "col-md-1 saveBtn"});
-//save to local storage
-function saveEvent(){
+//todaysDate();
+
+//save user-inputted reminder data to local storage
+function saveReminder(){
     localStorage.setItem("dayBlocks", JSON.stringify(dayBlocks));
 }
-//save to local storage
-$(".saveBtn").on('click', function(event){
-    event.preventDefault();
 
+//push data from local storage to viewable on the page
+function displayReminders(){
+    dayBlocks.forEach(function(_dayBlocks) {
+        $(`#${_dayBlocks.id}`).val(_dayBlocks.reminder);
+    })
+}
+//displays local storage data in view
+function finalDisplay() {
+    var dayStorage = JSON.parse(localStorage.getItem("dayBlocks"));
+    if (dayStorage){
+        dayBlocks = dayStorage;
+    }
+    saveReminder();
+    displayReminders();
+}
+// display todays date
+todaysDate();
+
+//visually creates overall timeblocks w/ times and events appended
+dayBlocks.forEach(function (hourBlock) {
+//row of the time blocks for each hour
+    var hourRow = $("<form>").attr({"class": "row"});
+    $(".container").append(hourRow);
+//area to display time
+    var timeField = $("<div>")
+    .text(`${hourBlock.hour}${hourBlock.ampm}`)
+    .attr({"class": "col-md-2 hour"});
+//scheduling data
+    var hourPlan = $("<div>")
+        .attr({"class": "col-md-9 description p-0"});
+    var planData = $("<textarea>");
+    hourPlan.append(planData);
+    planData.attr("id", hourBlock.id);
+//checks the current time with the time block  
+    if (hourBlock.time < moment().format("HH")){
+        planData.attr({
+            "class": "past"
+        })
+    } else if (hourBlock.time === moment().format("HH")){
+        planData.attr({
+            "class": "present"
+        })
+    } else if (hourBlock.time > moment().format("HH")){
+        planData.attr({
+            "class": "future"
+        })
+    }
+//create save button
+    var saveButton = $("<i class='far fa-save fa-lg'></i>")
+    var savePlan = $("<button>")
+        .attr({"class": "col-md-1 saveBtn"});
+    savePlan.append(saveButton);
+    hourRow.append(timeField, hourPlan, savePlan);
+})
+
+// load local storage data
+finalDisplay();
+
+//save to local storage
+$(".saveBtn").on("click", function(event){
+    event.preventDefault();
+    var arrayIndex = $(this).siblings(".description").children(".future").attr("id");
+    dayBlocks[arrayIndex].reminder = $(this).siblings(".description").children(".future").val();
+    console.log(arrayIndex);
+    saveReminder();
+    displayReminders();
 })
